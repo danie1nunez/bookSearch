9bf1e3733df8c602ef29b0573bd7d8abdd2d81ab
Daniel Nunez <161102831+danie1nunez@users.noreply.github.com>
2024-08-06 23:56:48 -0400

    added all the backend code necessary for project, including typeDef and resolvers, serverJs and auth

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,45 @@
+const { User, Book }  = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+   Query: {
+      me: async (parent, args, context) => {
+         if(context.user) {
+            return User.findOne({_id: context.user._id})
+         }
+   },
+  },
+
+  Mutation: {
+      addUser: async (parent, {username, email, password}) => {
+         const user = await User.create({ username, email, password });
+         const token = signToken(user);
+         return { token, user };
+      },
+      login: async (parent, { email, password }) => {
+         const user = await User.findOne({ email });
+
+         if(!user) {
+            throw AuthenticationError;
+         };
+         const token = signToken(user);
+         return { token, user };
+      },
+      saveBook: async (parent, {bookId, title, authors, description, image, link}, context) => {
+         if(context.user){
+            return Book.findOneAndUpdate(
+               {_id: user._id},
+               {
+                 $addToSet: {
+                   savedBooks: {bookId, title, authors, description, image, link},
+                 }
+               },
+               { new: true, runValidators: true}
+            )
+         }
+         throw AuthenticationError
+      }
+  }
+}
+
+module.exports =resolvers;
